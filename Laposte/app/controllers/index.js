37d2420b55a773a7501c62f9fdb0ba997a5e1a2e
friklergit <toanquach@iPhones-Mac-mini.local>
2014-07-28 15:00:16 +0700

    add resource

--- Laposte/app/controllers/index.js
+++ Laposte/app/controllers/index.js
@@ -1,5 +1,26 @@
-function doClick(e) {
-    alert($.label.text);
-}
+// --------------------------------------------
+var matrix2d = Ti.UI.create2DMatrix();
+matrix2d = matrix2d.rotate(180); // in degrees
+var a = Ti.UI.createAnimation({
+    transform: matrix2d,
+    duration: 1000,
+    autoreverse: true,
+    repeat: 100
+});
 
+// --------------------------------------------- 
+var menu = function(e){
+	var menuView = Alloy.createController('mainView').getView();	
+
+	menuView.open();
+};
+
+setTimeout(function()
+{
+    menu(null);
+ 	$.index.close();  
+},2000);
+
+
+$.spiner.animate(a);
 $.index.open();
